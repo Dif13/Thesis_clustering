2d7e808f6f7bce06251b4f0d74c049f873a41def
Ruzal Gimazov <diff@etersoft.ru>
2021-07-22 21:27:08 +0300

    Update prepare_data.py
    Create dict_file with uniq keywords and oecds (and index)

--- prepare_data.py
+++ prepare_data.py
@@ -58,7 +58,7 @@ def prepare_csv(df_name, output_file):
 
         temp = ''
         for k in keyword.split(sep=sep):
-            temp += k.strip() + ''
+            temp += k.strip() + chr(0x1f)
         if temp:
             temp = temp[:-1]
         split.append(temp)
@@ -68,8 +68,37 @@ def prepare_csv(df_name, output_file):
     df.to_csv(output_file)
 
 
+def create_dict(df_name, dict_file):
+    """
+    If function used separator: '' (ascii-code: 0x1f)
+    :param df_name:
+    :param dict_file:
+    :return:
+    """
+    df_source = pd.read_csv(df_name, index_col=0)
+    df_dict = pd.DataFrame()
+    list_uniq_keywords = []
+    for i, keyword in enumerate(df_source['keyword_list.0']):
+        list_uniq_keywords += keyword.split(sep=chr(0x1f))
+        if i % 1000 == 0:
+            print(i)
+
+    list_uniq_keywords = sorted(list(set(list_uniq_keywords)))
+    df_dict.index.name = 'index'
+    df_dict['uniq_keywords'] = list_uniq_keywords
+    list_uniq_oecds = sorted(list(set(df_source['oecds.0'])))
+    list_uniq_oecds += [np.NaN] * (len(list_uniq_keywords) - len(list_uniq_oecds))
+    df_dict['uniq_oecds'] = list_uniq_oecds
+    df_dict.to_csv(dict_file)
+
+
+
+
+
+
+
 def create_new_csv(df_name, output_file):
-    df_source = pd.read_csv(df_name, sep=',')
+    df_source = pd.read_csv(df_name)# , sep=',')
     df = pd.DataFrame()
     #df[['keyword_list.0', 'oecds.0']] = df_source[['keyword_list.0', 'oecds.0']]
     df.index.name = 'index'
@@ -127,21 +156,24 @@ def create_test_csv():
 
 
 def open_explorer():
-    input_file = easygui.fileopenbox(default=root_dir, filetypes=["*.csv"])
+    # input_file = easygui.fileopenbox(default=root_dir, filetypes=["*.csv"])
     # input_file = root_dir + "dissertation.csv"
     # input_file = root_dir + "dissertation_test.csv"
-    # input_file = root_dir + "key_words_oecds.csv"
+    #input_file = root_dir + "key_words_oecds.csv"
+    input_file = root_dir + "key_words_oecds_prepared.csv"
     return input_file
 
 
 def main():
     logger.info(f"Start {__name__}")
     df_name = open_explorer()
-    output_file = "key_words_oecds.csv"
-    output_file_split = "key_words_oecds_prepared.csv"
+    output_file = root_dir + "key_words_oecds.csv"
+    output_file_split = root_dir + "key_words_oecds_prepared.csv"
+    dict_file = root_dir + "dict_keywords_oecds.csv"
     # create_test_csv()
     # create_new_csv(df_name, output_file)
-    prepare_csv(output_file, output_file_split)
+    # prepare_csv(output_file, output_file_split)
+    create_dict(df_name, dict_file)
 
 
 if __name__ == '__main__':
